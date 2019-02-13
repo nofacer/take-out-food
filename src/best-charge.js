d114408967b26627bc5ee97859cbde240128930c
nofacer <nofacer@outlook.com>
2019-02-13 15:03:37 +0800

    get id and amount from input

---
+++ src/best-charge.js
@@ -0,0 +1,18 @@
+function bestCharge(selectedItems) {
+  return `
+============= 订餐明细 =============
+黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元
+-----------------------------------
+使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元
+-----------------------------------
+总计：25元
+===================================`.trim();
+}
+
+function decode(singleInstruction) {
+  const slicedInstruction = singleInstruction.split(" ");
+  return [slicedInstruction[0], parseInt(slicedInstruction[2])]
+}
